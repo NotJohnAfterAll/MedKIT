d76d13f1e66b09ec174439b2fb12ba81462e6c1e
NotJohnAfterall <145935737+NotJohnAfterall@users.noreply.github.com>
2025-03-18 09:18:59 +0100

    Added automatic video convertor as convertor in menu, other media types will be followed

--- convertor.py
+++ convertor.py
@@ -1,47 +1,71 @@
 import tkinter.filedialog
+import tkinter as tk
 import ffmpeg
 import tkinter
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
+import os
+import sys
+
+sys.coinit_flags = 2  # COINIT_APARTMENTTHREADED
 
 def getFile():
+    print("Function started")
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    root.update()
+    root.call('wm', 'attributes', '.', '-topmost', True)
+    print("update")
     file = tkinter.filedialog.askopenfilename(filetypes=[("Media", ".mp4 .mov .mkv .avi .webm")])
-    if file != "":
-        return file
-    else:
-        print("Error occured")
+
+    if file == "":
+        raise ValueError("File is empty")
+    
+    return file
 
 def setTargetFormat(format):
     global targetFormat
     targetFormat = format
 
-def convertVideo():
+def ConvertVideo():
     formats = ["mp4", "mov", "mkv", "avi", "webm"]
     file = getFile()
-    fileFormat = file.split(".")
-    fileFormat = fileFormat[1].lower()
+    fileFormat = file.split(".")[-1].lower()
     formats.remove(fileFormat)
     userSelection(formats)
+    print("past user selecion")
 
-    print(targetFormat) //DOESNT WORK
+    if targetFormat == "":
+        raise ValueError("Invalid File")
+
+    print("past target check")
+    convert(file, targetFormat)
+
+def convert(filepath, targetFormat):
+    workdir = os.path.dirname(filepath)
+    filename = os.path.basename(filepath)
+    filename = filename.rsplit(".", 1)[0]
+    filename = f"{filename}.{targetFormat}"
+    os.chdir(workdir)
+
+    input = ffmpeg.input(filepath, hwaccel='cuda')
+    ffmpeg.output(input, filename).run()
 
 def userSelection(formats):
     formatsClean = ' '.join(map(str, formats))
     print(f"Select to what format you want to convert: {formatsClean}")
     options = {format: (lambda fmt=format: setTargetFormat(fmt)) for format in formats}
-    print(targetFormat)
+    
     completer = WordCompleter(options.keys(), ignore_case=True)
     
     while True:
         choice = prompt("Select an option: ", completer=completer).strip().lower()
-        if choice in options:
-            options[choice]()
-            exit()
-        else:
-            print("Invalid choice. Try again.")
+
+        if choice not in options:
+            raise ValueError("Invalid choice. Try again")
+        options[choice]()
+        break
     
 
-
-
 if __name__ == "__main__":
-    convertVideo()
+    ConvertVideo()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import downloader as dl
+import convertor as cc
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
+import threading
 
 def downloadVideo():
     
@@ -25,7 +27,7 @@ def downloadAudio():
     print("Downloading Audio...")
 
 def convert():
-    print("Converting...")
+    threading.Thread(target=cc.ConvertVideo()).start()
 
 def transcode():
     print("Transcoding...")
@@ -36,11 +38,11 @@ def exit_program():
 
 def main_menu():
     options = {
-        "DV": downloadVideo,
-        "DA": downloadAudio,
-        "CC": convert,
-        "TS": transcode,
-        "Exit": exit_program
+        "downloadvideo": downloadVideo,
+        "downloadaudio": downloadAudio,
+        "convert": convert,
+        "transcode": transcode,
+        "exit": exit_program
     }
 
     completer = WordCompleter(options.keys(), ignore_case=True)
